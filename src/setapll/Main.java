b5e54444c3ebaa7839bb792396dc7f35f250719c
szaman693 <Rszard693@gmail.com>
2012-02-21 12:56:28 +0100

    13:00-2012-02-21

--- src/setapll/Main.java
+++ src/setapll/Main.java
@@ -17,14 +17,16 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        string();
+
+    }
+
+    private static void string() {
         // TODO code application logic here
-
         String[] a = {"a", "b", "c"};
-
         for (String string : a) {
             System.out.print(string);
-            
         }
-        System.out.println("\ntyle");
+
     }
 }
