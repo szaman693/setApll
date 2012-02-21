f669da145131824f69130602542ca0ba8ea0712c
szaman693 <szaman693@89.187.250.10>
2012-02-21 12:06:40 +0100

    2012-02-21

---
+++ src/setapll/Main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package setapll;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author szaman693
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        String[] a = {"a", "b", "c"};
+
+        for (String string : a) {
+            System.out.print(string);
+            
+        }
+        System.out.println("\ntyle");
+    }
+}
